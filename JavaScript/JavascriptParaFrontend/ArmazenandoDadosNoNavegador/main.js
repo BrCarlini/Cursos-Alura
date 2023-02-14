b659e7229340df25617ec604c94c2d419ebfad5d
Bruno Carlini <bruno-carlini@hotmail.com>
2023-02-13 21:14:01 -0300

    ArmazenamentoWeb: Adicionado funções de atualizar e remover item

--- JavaScript/JavascriptParaFrontend/ArmazenandoDadosNoNavegador/main.js
+++ JavaScript/JavascriptParaFrontend/ArmazenandoDadosNoNavegador/main.js
@@ -1,8 +1,8 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || [] 
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach( (elemento) => {
+itens.forEach((elemento) => {
     criaElemento(elemento)
 })
 
@@ -13,16 +13,34 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    
+    const existe = itens.find(elemento => elemento.nome === nome.value)
 
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+
+    }
+
+
+
+
+
+
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -32,18 +50,40 @@ form.addEventListener("submit", (evento) => {
 })
 
 
-function criaElemento(item){
+function criaElemento(item) {
     const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
 
+    numeroItem.dataset.id = item.id
+
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild(botaoDeleta())
+
     lista.appendChild(novoItem)
 
-    
+}
 
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerHTML = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag){
+    tag.remove()
 }
