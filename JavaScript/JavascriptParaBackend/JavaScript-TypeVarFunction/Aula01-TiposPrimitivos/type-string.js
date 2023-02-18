419334bc237b5b172ffcef700ccb299a778ba79f
Bruno Carlini <bruno-carlini@hotmail.com>
2023-02-17 22:58:26 -0300

    JsBack-TypeVarFunction: Visto tipo boolean

---
+++ JavaScript/JavascriptParaBackend/JavaScript-TypeVarFunction/Aula01-TiposPrimitivos/type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá, Mundo";
+const texto2 = 'Olá, Mundo';
+
+const senha = "senhaSuperSegura456";
+
+const stringDeNumeros = "34567";
+
+const citacao = 'fulando disse "oi!"';
+
+console.log(citacao)
+
+// concatenação
+console.log("Bruno disse: " + texto1);
+
+
+// template string ou template literal
+console.log(`Bruno disse: ${texto1}`);
