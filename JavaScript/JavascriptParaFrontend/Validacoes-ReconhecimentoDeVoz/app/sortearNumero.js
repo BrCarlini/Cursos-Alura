a8133c754c6dd9c8c045e398eac70d228f4723cd
Bruno Carlini <bruno-carlini@hotmail.com>
2023-03-09 21:13:55 -0300

    ReconhecimentoVoz: Realizado a validação do numero informado pelo reconhecimento de voz, verificado se é um numero, reconhecimento de voz deixado ativo automaticamente

--- JavaScript/JavascriptParaFrontend/Validacoes-ReconhecimentoDeVoz/app/sortearNumero.js
+++ JavaScript/JavascriptParaFrontend/Validacoes-ReconhecimentoDeVoz/app/sortearNumero.js
@@ -1,11 +1,13 @@
-const numeroSecreto = gerarNumeroAleatorio
 const menorValor = 1
-const maiorValor = 100
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
 
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * maiorValor + 1)
 }
+
+console.log(numeroSecreto)
 
 const elementoMenorValor = document.getElementById("menor-valor")
 elementoMenorValor.innerHTML = menorValor
