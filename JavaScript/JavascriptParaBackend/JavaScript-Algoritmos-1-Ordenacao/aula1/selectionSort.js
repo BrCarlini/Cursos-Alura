b9401db0510526c3bc55a859407c0b736a2245db
Bruno Carlini <bruno-carlini@hotmail.com>
2023-03-15 21:58:55 -0300

    JsBack-Ordenação: Visto os algoritmos SelectionSort e InsertionSort

--- JavaScript/JavascriptParaBackend/JavaScript-Algoritmos-1-Ordenacao/aula1/selectionSort.js
+++ JavaScript/JavascriptParaBackend/JavaScript-Algoritmos-1-Ordenacao/aula1/selectionSort.js
@@ -1,6 +1,38 @@
 const livros = require('./listaLivros');
 const menorValor = require('./menorValor');
 
-for (let atual = 0; atual < livros.length; atual++){
+
+/*
+Selection Sort com For tradicional
+
+for (let atual = 0; atual < livros.length - 1; atual++){
+    let menor = menorValor(livros, atual)
+
+    let livroAtual = livros[atual];
+    console.log('posicao  atual', atual)
+    console.log('livro atual', livros[atual])
     
+    let livroMenorPreco = livros[menor];
+    console.log('livro menor', livros[menor])
+
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
 }
+*/
+
+
+// Selection Sort com forEach
+
+livros.forEach((_, indice) => {
+
+    let menor = menorValor(livros, indice)
+
+    let livroAtual = livros[indice];
+    let livroMenorPreco = livros[menor];
+
+
+    livros[indice] = livroMenorPreco;
+    livros[menor] = livroAtual;
+})
+
+console.log(livros)
