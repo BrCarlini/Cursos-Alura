fb6eb9b69ae36e78d6e4fe57127ad4c8aa03799e
Bruno Carlini <bruno-carlini@hotmail.com>
2023-03-17 14:13:47 -0300

    Consumo-API: Curso finalizado, com o Consumo da API via CEP, realizado tratamento de dados e preenchimento automatico

--- JavaScript/JavascriptParaFrontend/ConsumindoTratando-Dados-API/script.js
+++ JavaScript/JavascriptParaFrontend/ConsumindoTratando-Dados-API/script.js
@@ -1,15 +1,32 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro){
-            throw Error('Esse cep não existe!')
-        }else {
-            console.log(r)
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consoleCepConvertida = await consultaCep.json();
+
+        if(consoleCepConvertida.erro) {
+            throw Error ('CEP não existente!');
         }
-        
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consoleCepConvertida.localidade;
+        logradouro.value = consoleCepConvertida.logradouro;
+        estado.value = consoleCepConvertida.uf;
+
+        console.log(consoleCepConvertida)
+
+        return consoleCepConvertida;
+
+    } catch(erro){
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro)
+    }
+}
 
 
-console.log(consultaCep)
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
