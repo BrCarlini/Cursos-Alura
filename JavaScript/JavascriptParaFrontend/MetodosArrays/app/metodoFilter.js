784ffd5b42c2ac09f312ae276c14f558e92116af
Bruno Carlini <bruno-carlini@hotmail.com>
2023-03-22 11:32:58 -0300

    MetodosArray: Visto os metodos Reduce e Sort, treinamento finalizado

--- JavaScript/JavascriptParaFrontend/MetodosArrays/app/metodoFilter.js
+++ JavaScript/JavascriptParaFrontend/MetodosArrays/app/metodoFilter.js
@@ -2,10 +2,42 @@ const btnTagCategoria = document.querySelectorAll('.btn');
 
 btnTagCategoria.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
-function filtrarLivros(){
+function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
+
+
 
     exibirOsLivrosNaTela(livrosFiltrados)
+
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLIvrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+
+
+function exibirValorTotalDosLIvrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    
+    `
 }
