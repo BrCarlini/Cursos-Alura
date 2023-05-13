3a9b5c4a2eb4873f0c1f631836bbdcc302704894
Bruno Carlini <bruno-carlini@hotmail.com>
2023-05-13 17:50:26 -0300

    Visto Polimorfismo, o que é SOLID e como aplicar, entendimento sobre interface

--- JavaScript/JavascriptParaBackend/JavaScript-ProgramacaoOrientadaObjeto/index.js
+++ JavaScript/JavascriptParaBackend/JavaScript-ProgramacaoOrientadaObjeto/index.js
@@ -3,4 +3,6 @@ import Docente from "./Docente.js";
 import User from "./User.js";
 
 const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
-console.log(novoUser.exibirInfos())
+console.log(novoUser.nome)
+novoUser.nome = 'Lucas'
+//console.log(novoUser.nome)
